f74c552219cc8b6b3fb9e947f08c42243e7a9290
Ram <guptaram94@gmail.com>
2023-02-22 15:30:12 +0530

    create design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function FilterableProductTable()
+{
+  return (
+    <div className='filterableproducttable'>
+      <SearchBar />
+      <ProductTable />
+    </div>
+  );
+}
+function ProductTable()
+{
+  return (
+    <div className='producttable'>
+      <h4>ProductTable area</h4>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th colSpan="2">ProductCategoryRow</th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+function SearchBar()
+{
+  return (
+    <div className='searchBar'>
+      <h4>SearchBar area</h4>
+      <form>
+        <input type="text" name="" placeholder='Search ....' />
+        <label>
+          <input type="checkbox" /> Only show products in stock
+        </label>
+      </form>
+    </div>
+  );
+}
+const PRODUCTS = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+];
+function App()
+{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>Welcome to mockup app</h4>
+      <FilterableProductTable />
+
+
     </div>
   );
 }
